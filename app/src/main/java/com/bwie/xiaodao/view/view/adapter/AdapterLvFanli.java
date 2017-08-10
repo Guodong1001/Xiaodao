bdac13356db2beca0512e08b37178cbf92729456
雨夜 <1241518725@qq.com>
2017-08-10 22:21:53 +0800

    李迎港

---
+++ app/src/main/java/com/bwie/xiaodao/view/view/adapter/AdapterLvFanli.java
@@ -0,0 +1,71 @@
+package com.bwie.xiaodao.view.view.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.bwie.xiaodao.R;
+
+import java.util.List;
+
+/**
+ * Created by 雨夜 on 2017/8/10.
+ */
+
+public class AdapterLvFanli extends BaseAdapter {
+    private Context mContext;
+    private List<String> mList;
+    private int itemCount = 2;
+
+    public AdapterLvFanli(Context context, List<String> list) {
+        mContext = context;
+        mList = list;
+    }
+
+    @Override
+    public int getCount() {
+        if (mList.size() > 2)
+        {
+            return itemCount;
+        }else
+        {
+            return mList.size();
+        }
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        viewHolder holder = null;
+        if (convertView==null){
+            convertView = View.inflate(mContext,R.layout.item_lv_fanli,null);
+            holder = new viewHolder();
+            holder.text = (TextView) convertView.findViewById(R.id.fanli_lv_item_txt_level);
+            convertView.setTag(holder);
+        }else {
+            holder = (viewHolder) convertView.getTag();
+        }
+        holder.text.setText(mList.get(position));
+        return convertView;
+    }
+
+    class viewHolder {
+        TextView text;
+    }
+    public void addItemNum(int number)
+    {
+        itemCount = number;
+    }
+}
+
--- app/src/main/java/com/bwie/xiaodao/view/view/fragment/FanliFragment.java
+++ app/src/main/java/com/bwie/xiaodao/view/view/fragment/FanliFragment.java
@@ -2,18 +2,64 @@ package com.bwie.xiaodao.view.view.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bwie.xiaodao.R;
+import com.bwie.xiaodao.view.view.adapter.AdapterLvFanli;
+import com.bwie.xiaodao.view.view.custom.MyListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FanliFragment extends Fragment {
 
+
+    @BindView(R.id.fanli_img_title)
+    ImageView mFanliImgTitle;
+    @BindView(R.id.fanli_txt_money)
+    TextView mFanliTxtMoney;
+    @BindView(R.id.fanli_txt_stroke_count)
+    TextView mFanliTxtStrokeCount;
+    @BindView(R.id.fanli_txt_query)
+    TextView mFanliTxtQuery;
+    @BindView(R.id.fanli_txt_weixin_bind)
+    TextView mFanliTxtWeixinBind;
+    @BindView(R.id.fanli_txt_alipay_bind)
+    TextView mFanliTxtAlipayBind;
+    @BindView(R.id.fanli_img_pay_more)
+    ImageView mFanliImgPayMore;
+    @BindView(R.id.fanli_txt_plan_count)
+    TextView mFanliTxtPlanCount;
+    @BindView(R.id.fanli_lv_details)
+    MyListView mFanliLvDetails;
+    @BindView(R.id.fanli_img_calendar)
+    ImageView mFanliImgCalendar;
+    @BindView(R.id.fanli_txt_rebate_7)
+    TextView mFanliTxtRebate7;
+    @BindView(R.id.fanli_txt_date)
+    TextView mFanliTxtDate;
+    @BindView(R.id.fanli_txt_money_amounts)
+    TextView mFanliTxtMoneyAmounts;
+    @BindView(R.id.fanli_img_goto_calendar)
+    ImageView mFanliImgGotoCalendar;
+    Unbinder unbinder;
+    private View view;
+    private List<String> mList;
+    private AdapterLvFanli mAdapterLvFanli;
 
     public FanliFragment() {
         // Required empty public constructor
@@ -24,7 +70,45 @@ public class FanliFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fanli, container, false);
+        view = inflater.inflate(R.layout.fragment_fanli, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        mList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            mList.add("item" + i);
+        }
+    }
+
+    private void initView() {
+        mFanliTxtPlanCount.setText("返利计划（共" + mList.size() + "档）");
+        mAdapterLvFanli = new AdapterLvFanli(getContext(), mList);
+        mFanliLvDetails.setAdapter(mAdapterLvFanli);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.fanli_txt_show_more)
+    public void onViewClicked() {
+        if (mAdapterLvFanli.getCount() == 2) {
+            mAdapterLvFanli.addItemNum(mList.size());
+            mAdapterLvFanli.notifyDataSetChanged();
+        } else {
+            mAdapterLvFanli.addItemNum(2);
+            mAdapterLvFanli.notifyDataSetChanged();
+        }
+    }
 }
