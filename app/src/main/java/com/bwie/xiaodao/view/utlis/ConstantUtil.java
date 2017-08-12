b374477745ce2839e640a24e8b332a832d474013
lifengzhuang <1327475176@qq.com>
2017-08-12 08:36:11 +0800

    李冯壮我的页面点击跳转

---
+++ app/src/main/java/com/bwie/xiaodao/view/utlis/ConstantUtil.java
@@ -0,0 +1,10 @@
+package com.bwie.xiaodao.view.utlis;
+
+/**
+ * Created by 李冯壮 on 2017/8/11.
+ */
+
+public class ConstantUtil {
+    //登录状态
+    public static final boolean LOGINSTATUS = false;
+}
