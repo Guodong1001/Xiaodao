1ef4c9fca7fa88312544e71914a89949b954a276
liuxiaoqian1 <2539563561@qq.com>
2017-08-12 10:16:09 +0800

    Merge branches 'Liuxiaoqian' and 'lifengzhuang' of https://github.com/Guodong1001/Xiaodao into Liuxiaoqian
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/bwie/xiaodao/view/view/fragment/FujinFragment.java
+++ app/src/main/java/com/bwie/xiaodao/view/view/fragment/FujinFragment.java
@@ -12,11 +12,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
 import com.bwie.xiaodao.R;
+import com.bwie.xiaodao.view.view.activity.MyLocationListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,10 +34,17 @@ public class FujinFragment extends Fragment{
     private View view;
     private MapView mMapView;
     private BaiduMap mBaiduMap;
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         SDKInitializer.initialize(getActivity().getApplicationContext());
         view = View.inflate(getActivity(), R.layout.fragment_fujin, null);
+        mLocationClient = new LocationClient(getActivity().getApplicationContext());
+        mLocationClient.registerLocationListener( myListener );
+        addGPS(view);
+        initLocation();
+
         //获取地图控件引用
         mMapView = (MapView)view. findViewById(R.id.bmapView);
         mRadioGroup = (RadioGroup) view.findViewById(R.id.fujin_rg);
@@ -54,8 +66,54 @@ public class FujinFragment extends Fragment{
         });
         return view;
     }
+    public void addGPS(View view){
+        Toast.makeText(getActivity(),"2",Toast.LENGTH_SHORT).show();
+        mLocationClient.start();
+        //mLocationClient.stop();
+    }
 
-//    @Override
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
+        //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+
+        option.setCoorType("bd09ll");
+        //可选，默认gcj02，设置返回的定位结果坐标系
+
+        int span=0;
+        option.setScanSpan(span);
+        //可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+
+        option.setIsNeedAddress(true);
+        //可选，设置是否需要地址信息，默认不需要
+
+        option.setOpenGps(true);
+        //可选，默认false,设置是否使用gps
+
+        option.setLocationNotify(true);
+        //可选，默认false，设置是否当GPS有效时按照1S/1次频率输出GPS结果
+
+        option.setIsNeedLocationDescribe(true);
+        //可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+
+        option.setIsNeedLocationPoiList(true);
+        //可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+
+        option.setIgnoreKillProcess(false);
+        //可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+
+        option.SetIgnoreCacheException(false);
+        //可选，默认false，设置是否收集CRASH信息，默认收集
+
+        option.setEnableSimulateGps(false);
+        //可选，默认false，设置是否需要过滤GPS仿真结果，默认需要
+
+        mLocationClient.setLocOption(option);
+
+    }
+
+
+    //    @Override
 //    public void onDestroy() {
 //        super.onDestroy();
 //        mMapView.onDestroy();
