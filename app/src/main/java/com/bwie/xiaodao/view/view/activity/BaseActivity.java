6c2bb9ad68d3b7ae952bcbbc53f4af13e20788a3
1441610164@qq.com <1441610164@qq.com>
2017-08-14 08:37:44 +0800

    修改之后的布局

--- app/src/main/java/com/bwie/xiaodao/view/view/activity/BaseActivity.java
+++ app/src/main/java/com/bwie/xiaodao/view/view/activity/BaseActivity.java
@@ -13,6 +13,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(setMyContentView());
         initView();
         initData();
