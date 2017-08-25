7ad3b124391b817946d5179c78917e0197b05d3f
LemonXue <1243841954@qq.com>
2017-08-25 11:40:09 +0800

    合并了组内人员李冯壮的代码

--- app/src/main/java/com/bwie/xiaodao/view/view/adapter/HomeClassShowAdapter.java
+++ app/src/main/java/com/bwie/xiaodao/view/view/adapter/HomeClassShowAdapter.java
@@ -9,14 +9,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bwie.xiaodao.R;
 import com.bwie.xiaodao.view.model.bean.GoodsShowBean;
 import com.bwie.xiaodao.view.utlis.DpUtils;
 
 import java.util.List;
-
-import jp.wasabeef.glide.transformations.MaskTransformation;
 
 /**
  * 类描述：
@@ -37,7 +34,7 @@ public class HomeClassShowAdapter extends RecyclerView.Adapter<HomeClassShowAdap
     @Override
     public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(mContext).inflate(R.layout.item_home_goods_rv,parent,false);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.item_home_goods_rv, parent, false);
         view.setOnClickListener(this);
         return new MyHolder(view);
     }
@@ -50,9 +47,9 @@ public class HomeClassShowAdapter extends RecyclerView.Adapter<HomeClassShowAdap
         int height = DpUtils.dip2px(mContext, 100.0f);
         Glide.with(mContext)
                 .load(bean.getPicture())
-                .override(width, height)
-                .bitmapTransform(new CenterCrop(mContext),
-                        new MaskTransformation(mContext, R.drawable.rounded_rectangle))
+//                .override(width, height)
+//                .bitmapTransform(new CenterCrop(mContext),
+//                        new MaskTransformation(mContext, R.drawable.rounded_rectangle))
                 .into(holder.goodsImg);
 //        Glide.with(mContext)
 //                .load(bean.getPicture())
@@ -60,8 +57,8 @@ public class HomeClassShowAdapter extends RecyclerView.Adapter<HomeClassShowAdap
 //                        RoundedCornersTransformation.CornerType.BOTTOM))
 //                .into(holder.goodsImg);
         holder.goodsName.setText(bean.getShopName());
-        holder.goodsPrice.setText("￥"+bean.getPerCapitaConsumption()+"/人");
-        holder.goodsIntegral.setText("积分率"+bean.getIntegralRate()+"%");
+        holder.goodsPrice.setText("￥" + bean.getPerCapitaConsumption() + "/人");
+        holder.goodsIntegral.setText("积分率" + bean.getIntegralRate() + "%");
         holder.goodsDistance.setText(bean.getAddress());
         holder.itemView.setTag(position);
     }
@@ -76,19 +73,21 @@ public class HomeClassShowAdapter extends RecyclerView.Adapter<HomeClassShowAdap
     public void onClick(View v) {
         if (mOnItemClickListener != null) {
             //注意这里使用getTag方法获取position
-            mOnItemClickListener.onItemClick(v,(int)v.getTag());
+            mOnItemClickListener.onItemClick(v, (int) v.getTag());
         }
     }
+
     public void setOnItemClickListener(OnItemClickListener listener) {
         this.mOnItemClickListener = listener;
     }
 
-    class MyHolder extends RecyclerView.ViewHolder{
+    class MyHolder extends RecyclerView.ViewHolder {
         ImageView goodsImg;
         TextView goodsName;
         TextView goodsPrice;
         TextView goodsIntegral;
         TextView goodsDistance;
+
         public MyHolder(View itemView) {
             super(itemView);
             goodsImg = (ImageView) itemView.findViewById(R.id.item_goods_img);
@@ -98,8 +97,9 @@ public class HomeClassShowAdapter extends RecyclerView.Adapter<HomeClassShowAdap
             goodsDistance = (TextView) itemView.findViewById(R.id.item_goods_distance);
         }
     }
+
     public static interface OnItemClickListener {
-        void onItemClick(View view , int position);
+        void onItemClick(View view, int position);
     }
 
 }
